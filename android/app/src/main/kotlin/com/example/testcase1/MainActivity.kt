a8029d42378ebb48276fa41fa3a383a2fb5f113d
sanabilmohd12 <sanabilmohd9191@gmail.com>
2024-11-13 13:01:37 +0530

    by Sanabil

---
+++ android/app/src/main/kotlin/com/example/testcase1/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.testcase1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/utils/constants/colors.dart
@@ -0,0 +1,11 @@
+
+import 'dart:ui';
+
+const Color white = Color(0xFFFFFFFF);
+const Color textblack = Color(0xFF333333);
+const Color textblack2 = Color(0xFF000000);
+const Color textblack3 = Color(0x66000000);
+const Color textblue = Color(0xFF2873F0);
+const Color textred = Color(0xFFFF5454);
+const Color buttonblack = Color(0xFF100E09);
+const Color bggrey = Color(0xFFEBEBEB);
